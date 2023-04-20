10e72cc41f112d1ee78f7ae2d78481d2e5cafe99
erickguerra22 <erickguerra2201@gmail.com>
2023-04-19 20:16:22 -0600

    a

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './Home.css'
 // eslint-disable-next-line import/no-cycle
 import NavBar from '../../components/NavBar/NavBar'
 import Profile from '../../components/Profile/Profile'
-import List from '../../components/List/List'
 
 const Home = () => {
-  const [list, setList] = useState([])
   const randomColor = `rgb(${(Math.floor(Math.random() * 200))},${(Math.floor(Math.random() * 200))},${(Math.floor(Math.random() * 200))})`
   document.getElementById('title').innerHTML = 'Página principal'
   return (
@@ -15,7 +13,6 @@ const Home = () => {
       <div className="home">
         <Profile randomColor={randomColor} />
         {
-          //<List />
         }
       </div>
     </div>
